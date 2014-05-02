b12920ffcc91efc57addf6f566820323c47e5267
Yuji Kamiya <yuji.kamiya@gree.net>
2014-05-02 09:33:51 +0900

    add method

--- hs/http/http.hs
+++ hs/http/http.hs
@@ -1,7 +1,10 @@
 import Network.HTTP.Conduit
+import Data.Conduit
+import Data.Conduit.Binary (sinkFile)
 import System.Environment
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as C8
 
 main :: IO ()
 main = getArgs >>= printResponseHeader 
@@ -13,10 +16,21 @@ printResponseHeader urls = do
             case parseUrl urlString of
                 Nothing -> putStrLn "invalid url"
                 Just req -> withManager $ \manager -> do
-                    let reqHead = req { method = "HEAD" }
+                    let reqHead = req { method = L.toStrict $ C8.pack "HEAD" }
                     res <- http req manager
                     liftIO $ do
                         print $ responseStatus res
                         mapM_ print $ responseHeaders res
         _ -> putStrLn "Sorry, please provide example one URL"
 
+useSimpleHttp :: IO ()
+useSimpleHttp = do
+        simpleHttp "http://www.yesodweb.com/book/http-conduit" >>= L.writeFile "foo.txt"
+        
+effiecientHttp :: IO ()
+effiecientHttp = do
+        runResourceT $ do
+            manager <- liftIO $ newManager conduitManagerSettings
+            req <- liftIO $ parseUrl "http://www.yesodweb.com/book/http-conduit"
+            res <- http req manager
+            print $ responseBody res 
