80e07b6e2d6fa86bf3cdc6203d4970f0f547bc66
Yuji Kamiya <yuji.kamiya@gree.net>
2014-06-24 13:18:04 +0900

    heap

--- cpp/contest_challenge_book/heap.cpp
+++ cpp/contest_challenge_book/heap.cpp
@@ -3,6 +3,7 @@
  */
 #include <cstdio>
 #include <cstring>
+#include <iostream>
 #include <algorithm>
 #include <queue>
 #include <vector>
@@ -11,12 +12,33 @@ using namespace std;
 
 class Heap 
 {
-private:
-    vector<int> heap;
+    int sz;
+    int heap[100];
 public:
     void push(int x) {
+        int i = sz++;
+        while (i > 0) {
+            cout << i << endl;
+            int p = (i - 1) / 2;
+            if (heap[p] <= x) break;
+            heap[i] = heap[p];
+            i = p;
+        } 
+        heap[i] = x;
     }
     int pop() {
+        int ret = heap[0];
+        int x = heap[--sz];
+        int i = 0;
+        while (i * 2 + 1 < sz) {
+            int a = i * 2 + 1, b = i * 2 + 2;
+            if (b < sz && heap[b] < heap[a]) a = b;
+            if (heap[a] >= x) break;
+            heap[i] = heap[a];
+            i = a;
+        }
+        heap[i] = x;
+        return ret;
     }
 };
 
