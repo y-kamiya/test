1ca93df019b3d7749273bd31a2f6e6e2af18dff2
Yuji Kamiya <y.kamiya0@gmail.com>
2014-12-21 12:28:16 +0900

    fix bug in initialPointSums

--- hs/parallel-concurrent/src/Kmeans/Main.hs
+++ hs/parallel-concurrent/src/Kmeans/Main.hs
@@ -2,10 +2,11 @@ import Data.List
 import Data.Ord
 import Data.Tuple
 import Data.Binary 
+import Data.Functor 
 
 data Point = Point !Double !Double deriving (Eq, Show, Read)
 data PointSum = PointSum !Int !Double !Double           
-data Cluster = Cluster { clId :: Int, clCent :: Point } deriving (Eq, Show)
+data Cluster = Cluster { clId :: Int, clCent :: Point } deriving (Eq, Show, Read)
 
 instance Binary Point where
     put (Point a b) = put a >> put b
@@ -24,34 +25,32 @@ pointSumToCluster :: Int -> PointSum -> Cluster
 pointSumToCluster i (PointSum count x y) = Cluster i $ Point (x / n) (y / n)
   where n = fromIntegral count
 
-findClosestCluster :: [Cluster] -> Point -> Int
-findClosestCluster cs p = fst $ minimumBy (comparing snd) $ zip [0..] ds
+findClosestCluster :: [Cluster] -> Point -> Cluster
+findClosestCluster cs p = fst $ minimumBy (comparing snd) ds
   where
-    ds = map (sqDistance p) $ map clCent cs
+    ds = [(c, sqDistance p $ clCent c) | c <- cs]
 
 assign :: [Point] -> [Cluster] -> [Cluster]
 assign ps cs = map (uncurry pointSumToCluster) $ zip [0..] newPointSums
   where
     newPointSums = foldr (makeNewPointSums cs) initialPointSums ps
-    initialPointSums = take (length cs) $ infinitePointSums 0
-    infinitePointSums n = PointSum n 0 0 : infinitePointSums (n+1) 
+    initialPointSums = take (length cs) $ repeat $ PointSum 0 0 0
 
 makeNewPointSums :: [Cluster] -> Point -> [PointSum] -> [PointSum]
 makeNewPointSums cs p pss = take n pss ++ [psum] ++ drop (n+1) pss
   where
-    n = findClosestCluster cs p
+    n = clId $ findClosestCluster cs p
     psum = addToPointSum (pss!!n) p
 
 main :: IO ()
 main = do
     points <- decodeFile "points.bin"
-    let -- points = [Point 1.0 1.0, Point 1.0 2.0, Point 12.0 13.0, Point 13.0 11.0]
-        clusters = [Cluster 0 zeroPoint, Cluster 1 (Point 11.0 11.0)]
+    clusters <- read <$> readFile "clusters"
     r <- loop 0 points clusters
     print r
     where
       loop :: Int -> [Point] -> [Cluster] -> IO (Either String [Cluster])
-      loop 100 ps cs = return $ Left "not converged"
+      loop 100 _ _ = return $ Left "not converged"
       loop n ps cs = let cs' = assign ps cs 
                      in  case cs == cs' of
                            True -> return $ Right cs'
