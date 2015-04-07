4c8f6e1e62997b7abee32044400ab18a88a71e3a
Yuji Kamiya <yuji.kamiya@gree.net>
2015-04-07 10:04:58 +0900

    play with aeson

--- hs/aeson-test/main.hs
+++ hs/aeson-test/main.hs
@@ -6,9 +6,9 @@ import Data.Aeson
 import Data.Aeson.TH
 import Control.Applicative
 import Control.Monad
-import Data.Char (toLower)
-import Data.Text hiding (drop, map, toLower)
-import Data.Map hiding (map)
+import Data.Char (toLower, toUpper)
+import Data.Text hiding (drop, map, toLower, toUpper)
+import Data.Map hiding (map, size)
 import qualified Data.ByteString.Lazy.Char8 as C
 
 data Person = Person
@@ -41,8 +41,32 @@ main = do
            , testTwo = True
            , testThree = Product "test" 'A' 123
            } :: D Int
+        fruits = Fruits { color = "green", shape = "ball", size = 1 }
 
+    print $ toJSON d
     print $ fromJSON (toJSON d) == Success d
+    print $ (decode "{\"name\":\"Joe\",\"age\":12}" :: Maybe Person)
+    print $ (decode "{\"color\":\"red\",\"shape\":\"ball\",\"size\":1}" :: Maybe Fruits)
+    print $ toJSON fruits
+    print $ fromJSON (toJSON fruits) == Success fruits
 
 
+data Fruits = Fruits
+    { color :: String
+    , shape :: String
+    , size  :: Int
+    } deriving (Eq, Show)
 
+$(deriveJSON defaultOptions { fieldLabelModifier = drop 1, constructorTagModifier = map toUpper } ''Fruits)
+
+{-
+instance FromJSON Fruits where
+    parseJSON (Object v) = Fruits <$>
+                           v .: "color" <*>
+                           v .: "shape" <*>
+                           v .: "size"
+    parseJSON _ = mzero
+
+instance ToJSON Fruits where
+    toJSON (Fruits color shape size) = object [ "color" .= color, "shape" .= shape, "size" .= size ]
+-}
