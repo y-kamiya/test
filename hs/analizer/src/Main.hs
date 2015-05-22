01b5037bd03aec50c22b13d8a69fe43f9f95b7e8
Yuji Kamiya <yuji.kamiya@gree.net>
2015-05-22 09:59:27 +0900

    mkNexus for split

--- hs/analizer/src/Main.hs
+++ hs/analizer/src/Main.hs
@@ -16,10 +16,11 @@ analize :: [String] -> IO ()
 analize [file1, file2] = do
     content1 <- map readFloat <$> lines <$> readFile file1
     content2 <- map readFloat <$> lines <$> readFile file2
-    print $ "average of content1: " <> show (average content1)
-    print $ "average of content2: " <> show (average content2)
-    print $ "average of difference: " <> show (average $ zipWith (-) content2 content1)
-    print $ "variance of difference: " <> show (variance $ zipWith (-) content2 content1)
+    -- print $ "average of content1: " <> show (average content1)
+    -- print $ "average of content2: " <> show (average content2)
+    -- print $ "average of difference: " <> show (average $ zipWith (-) content2 content1)
+    -- print $ "variance of difference: " <> show (variance $ zipWith (-) content2 content1)
+    putStrLn $ show (average $ zipWith (-) content2 content1) <> "\t" <> show (variance $ zipWith (-) content2 content1)
 
 average :: [Float] -> Float
 average nums = sum nums / fromIntegral (length nums)
--- hs/pearls/src/Hylo/Main.hs
+++ hs/pearls/src/Hylo/Main.hs
@@ -1,6 +1,6 @@
 
 
-data Tree a = Leaf a | Node [Tree a]
+data Tree a = Leaf a | Node [Tree a] deriving Show
 
 fold :: (a -> b) -> ([b] -> b) -> Tree a -> b
 fold f g (Leaf x) = f x
@@ -10,5 +10,60 @@ unfold :: (b -> Bool) -> (b -> a) -> (b -> [b]) -> b -> Tree a
 unfold p v h x = if p x then Leaf (v x)
                         else Node $ map (unfold p v h) (h x)
 
+
+data LTree a = LLeaf a | LNode a [LTree a] deriving Show
+
+fill :: (a -> b) -> ([b] -> b) -> Tree a -> LTree b
+fill f g = fold (lleaf f) (lnode g)
+
+lleaf f x = LLeaf (f x)
+lnode g ts = LNode (g (map label ts)) ts
+
+label (LLeaf x) = x
+label (LNode x ts) = x
+
+hylo :: ([a] -> b) -> ([b] -> b) -> ([a] -> [[a]]) -> [a] -> b
+hylo f g h = fold f g . mkTree h
+
+mkTree h = unfold single id h
+
+single xs = length xs == 1
+
+-- three examples
+-- 1.
+split xs = [take n xs, drop n xs] where n = length xs `div` 2
+
+-- 2.
+isegs xs = [init xs, tail xs]
+
+recover xss = head (head xss) : last xss
+
+-- 3.
+minors [x,y] = [[x],[y]]
+minors (x:xs) = map (x:) (minors xs) ++ [xs]
+
+
+-- compose nexus
+mkNexus f g = label . extractL . until singleL (stepL g) . initialL f
+
+type Layer a = [a]
+
+initialL :: ([a] -> b) -> [a] -> Layer (LTree b)
+initialL f = map (lleaf f . wrap)
+wrap x = [x]
+
+singleL :: Layer (LTree b) -> Bool
+singleL = single
+
+extractL :: Layer (LTree b) -> LTree b
+extractL = head
+
+stepL :: ([b] -> b) -> Layer (LTree b) -> Layer (LTree b)
+stepL g = map (lnode g) . group
+
+group [] = []
+group (x:y:xs) = [x,y] : group xs
+
 main :: IO ()
-main = print "aa"
+main = print $ mkNexus id merge "abcd"
+  where merge = concat
