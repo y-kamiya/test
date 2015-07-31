87d096053cbc3cbb81597146cf37048ce6f2d98b
Yuji Kamiya <y.kamiya0@gmail.com>
2015-07-31 22:44:13 +0900

    monadt

--- hs/experiments/src/Monadt/Main.hs
+++ hs/experiments/src/Monadt/Main.hs
@@ -1,11 +1,19 @@
 module Main where
 
 import Control.Monad.State
+import Control.Monad.Reader
+import Control.Monad.State
+import Control.Monad.Trans.Either
 
 main :: IO ()
 main = do
   let st = StateT $ \s -> return ("test", s)
   print =<< runStateT st 0
+  (a, s) <- runStateT (runReaderT (runEitherT monadt) "reader") "state"
+  case a of
+    Left e -> print $ "left " ++ e
+    Right r -> print $ "right " ++ r
+  print s
 
 
 lift' :: Monad m => m a -> StateT s m a
@@ -18,3 +26,12 @@ modify' :: Monad m => (s -> s) -> StateT s m ()
 modify' f = StateT $ \s -> return ((), f s)
 
 a `bind` f = StateT $ \s -> runStateT a s >>= \(a', s') -> runStateT (f a') s'
+
+monadt :: EitherT e (ReaderT String (StateT s IO)) String
+monadt = EitherT $ ReaderT $ \r -> StateT $ \s -> return (Right (r ++ "IO"), s)
+
+-- statet :: StateT s IO a
+-- statet = StateT $ \s -> return (1, s)
+--
+-- readert :: ReaderT r IO a
+-- readert = ReaderT $ \r -> return (
