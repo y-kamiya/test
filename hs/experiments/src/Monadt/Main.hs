a066da6f2d101e679f89b410219df6e9d459398b
Yuji Kamiya <y.kamiya0@gmail.com>
2015-07-31 09:54:15 +0900

    move monadt to experiments

--- hs/experiments/src/Monadt/Main.hs
+++ hs/experiments/src/Monadt/Main.hs
@@ -8,8 +8,13 @@ main = do
   print =<< runStateT st 0
 
 
+lift' :: Monad m => m a -> StateT s m a
 lift' m = StateT $ \s -> m >>= \a -> return (a, s)
+
+get' :: Monad m => StateT s m s
 get' = StateT $ \s -> return (s,s)
-modify' f = StateT $ \s -> ((), f s)
+
+modify' :: Monad m => (s -> s) -> StateT s m ()
+modify' f = StateT $ \s -> return ((), f s)
 
 a `bind` f = StateT $ \s -> runStateT a s >>= \(a', s') -> runStateT (f a') s'
