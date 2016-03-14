f783cbd11d62797a034bb3a15b2173b224abea03
Yuji Kamiya <y.kamiya0@gmail.com>
2016-03-15 08:23:13 +0900

    messages

--- scala/ChatServer/src/main/scala/ChatHandler.scala
+++ scala/ChatServer/src/main/scala/ChatHandler.scala
@@ -4,20 +4,58 @@ import akka.actor.{ Actor, ActorRef, ActorPath, Props }
 import akka.io.{ IO, Tcp }
 import akka.util.ByteString
 import java.net.InetSocketAddress
+import scala.collection.mutable.HashMap
 
-class ChatHandler(list: List[ActorPath]) extends Actor {
+class ClientMap {
+  var map: HashMap[String, ActorPath] = new HashMap
+
+  def add(name: String, path: ActorPath) {
+    map += (name -> path)
+  }
+
+  def remove(name: String) {
+    map -= name
+  }
+}
+
+trait Message
+case class NewClient(name: String) with Message
+case class Broadcast(name: String, msg: String) with Message
+case class Tell(name: String, msg: String) with Message
+
+class ChatHandler(ref: ActorRef) extends Actor {
   import Tcp._
-  def receive = {
-    case Received(data) => 
-      sender() ! Write(data)
-      // var a = context.actorSelection(path)
-      println(list)
-      context.actorSelection("/user/server/*") ! "Message 1"
-      list.foreach(path => {
-        println(path)
-      })
+
+  var clientName: String
+
+  def confirmName = {
+    ref ! Write("What your name?")
+  }
+
+  def receive ={
+    case name: String => 
+      clientName = name
+      clientMap.add(name, sender().path)
+      nofifyJoin(name)
+      become("loggedIn")
+  }
+
+  def loggedIn = {
+    case Received(data) => broadcast(clientName, data)
     case PeerClosed => context.stop(self)
-    case m:String => println(m)
+    case Broadcast(name, str) => ref ! s"*** $name ***: $str"
+  }
+
+  def notifyJoin(name: String) {
+    nofifyAll(name, NewClient(name))
+  }
+
+  def broadcast(name: String, str: String) {
+    nofifyAll(name, Broadcast(name, str))
+  }
+
+  def nofifyAll(name: String, message: Message) {
+    context.actorSelection("/user/server/*") ! message
   }
 }
 
--- scala/ChatServer/src/main/scala/Server.scala
+++ scala/ChatServer/src/main/scala/Server.scala
@@ -6,23 +6,12 @@ import akka.util.ByteString
 import java.net.InetSocketAddress
 import scala.collection.mutable.ListBuffer
 
-class ServerState {
-  var clientList = new ListBuffer[ActorPath]
-
-  def addClient(actorPath: ActorPath) {
-    clientList += actorPath
-  }
-
-  def removeClient(actorPath: ActorPath) {
-  }
-}
-
 class Server extends Actor {
   import Tcp._
   import context.system
 
   IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", 8011))
-  var serverState = new ServerState
+  // var serverState = new ServerState
 
   def receive = {
     case b @ Bound(localAddress) =>
@@ -30,13 +19,8 @@ class Server extends Actor {
     case CommandFailed(command: Bind) =>
       println(command.failureMessage)
     case c @ Connected(remote, local) =>
-      val handler = context.actorOf(Props(classOf[ChatHandler], serverState.clientList.toList))
-      println(remote)
-      println(local)
-      println(c)
-      serverState.addClient(handler.path)
+      val handler = context.actorOf(Props(classOf[ChatHandler]), sender())
       sender() ! Register(handler)
-
   }
 }
 
