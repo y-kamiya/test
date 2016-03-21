8ffc2f47722eec14a5fd0e99a2c4da77c1647227
Yuji Kamiya <y.kamiya0@gmail.com>
2016-03-21 15:33:57 +0900

    name decision

--- scala/ChatServer/src/main/scala/ChatHandler.scala
+++ scala/ChatServer/src/main/scala/ChatHandler.scala
@@ -1,10 +1,11 @@
 package ChatServer
 
-import akka.actor.{ Actor, ActorRef, ActorPath, Props }
+import akka.actor.{ Actor, ActorRef, ActorPath, Props, ReceiveTimeout, PoisonPill }
 import akka.io.{ IO, Tcp }
 import akka.util.ByteString
 import java.net.InetSocketAddress
 import scala.collection.mutable.HashMap
+import scala.concurrent.duration._
 
 class ClientMap {
   val map: HashMap[String, ActorPath] = new HashMap
@@ -20,6 +21,19 @@ class ClientMap {
   def remove(name: String) {
     map -= name
   }
+
+  def size: Int = {
+    return map.size
+  }
+
+  def update(name: String, path: ActorPath) {
+    map(name) = path
+  }
+
+  def contains(name: String): Boolean = {
+    return map.contains(name)
+  }
+
 }
 
 trait ChatMessage
@@ -29,15 +43,17 @@ case class RemoveClient(name: String) extends ChatMessage
 case class Broadcast(name: String, msg: String) extends ChatMessage
 case class Tell(name: String, msg: String) extends ChatMessage
 case class Notice(msg: String) extends ChatMessage
+case class NameConfirmation(name: String) extends ChatMessage
+case class AckNameConfirmation(name: String, isOk: Boolean) extends ChatMessage
 
-class ChatHandler(ref: ActorRef) extends Actor {
+class ChatHandler(ref: ActorRef, memberCount: Int) extends Actor {
   import Tcp._
 
   val Crlf = "\r\n"
   val Separator = " "
 
   var clientName = "undefined"
-  val clientMap = new ClientMap
+  var clientMap = new ClientMap
 
   respondToClient(s"What your name?$Crlf")
 
@@ -46,8 +62,33 @@ class ChatHandler(ref: ActorRef) extends Actor {
       println("receive name")
       clientName = name.utf8String.stripLineEnd
       clientMap.add(clientName, sender().path)
-      notifyJoin(clientName)
-      context.become(loggedIn)
+      memberCount match {
+        case 0 => context.become(loggedIn)
+        case _ => 
+          notifyAll(NameConfirmation(clientName))
+          context.setReceiveTimeout(10 seconds)
+          context.become(naming)
+      }
+  }
+
+  def naming: Receive = {
+    case AckNameConfirmation(name, true) =>
+      println(s"success $clientName")
+      clientMap.add(name, sender().path)
+      if (memberCount / 2 < clientMap.size) {
+        context.setReceiveTimeout(Duration.Undefined)
+        notifyJoin(clientName)
+        context.become(loggedIn)
+      }
+    case AckNameConfirmation(_, false) => failedToNameDecision
+    case ReceiveTimeout => failedToNameDecision
+  }
+
+  def failedToNameDecision {
+    println(s"failed $clientName")
+    context.setReceiveTimeout(Duration.Undefined)
+    respondToClient(s"$clientName has already used$Crlf")
+    self ! PoisonPill
   }
 
   def loggedIn: Receive = {
@@ -69,11 +110,20 @@ class ChatHandler(ref: ActorRef) extends Actor {
           broadcast(clientName, str)
       }
     case PeerClosed => context.stop(self)
+    case NameConfirmation(name) if sender == self => noop
+    case NameConfirmation(name) =>
+      println(s"NameConfirmation in $clientName")
+      val exist = clientMap.contains(name)
+      sender() ! AckNameConfirmation(clientName, !exist)
+      if (!exist) {
+        clientMap.add(name, sender().path)
+      }
+
     case Broadcast(name, str) =>
       println("receive Broadcast")
       respondToClient(s"* $name *: $str$Crlf")
     case NewClient(name) =>
-      clientMap.add(name, sender().path)
+      clientMap.update(name, sender().path)
       if (name != clientName) {
         sender() ! AckNewClient(clientName)
         respondToClient(s"greet from $name$Crlf")
--- scala/ChatServer/src/main/scala/Server.scala
+++ scala/ChatServer/src/main/scala/Server.scala
@@ -10,6 +10,8 @@ class Server extends Actor {
   import Tcp._
   import context.system
 
+  var memberCount = 0
+
   IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", 8011))
 
   def receive = {
@@ -18,7 +20,9 @@ class Server extends Actor {
     case CommandFailed(command: Bind) =>
       println(command.failureMessage)
     case c @ Connected(remote, local) =>
-      val handler = context.actorOf(Props(classOf[ChatHandler], sender()))
+      val handler = context.actorOf(Props(classOf[ChatHandler], sender(), memberCount))
       sender() ! Register(handler)
+      memberCount += 1
+    case RemoveClient(name) => memberCount -= 1
   }
 }
