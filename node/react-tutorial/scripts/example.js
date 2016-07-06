551b20bdc286e248afb8c03cb6e08e2ed7cbd65a
Yuji Kamiya <y.kamiya0@gmail.com>
2016-07-07 08:23:59 +0900

    react tutorial

---
+++ node/react-tutorial/scripts/example.js
@@ -0,0 +1,61 @@
+var data = [
+   {id: 1, author: "Pete Hunt", text: "This is one comment"},
+   {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.props.data}/>
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>{comment.text}</Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    var md = new Remarkable();
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={ { __html: md.render(this.props.children.toString()) } } />
+      </div>
+    );
+  }
+});
+
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, CommentForm
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox data={data} />,
+  document.getElementById('content')
+);
