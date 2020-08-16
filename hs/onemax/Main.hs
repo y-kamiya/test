1f30ddf493d5f974ec3432def23a7f584e86aae6
Yuji Kamiya <y.kamiya0@gmail.com>
2020-08-16 22:21:07 +0900

    solve onemax problem

--- hs/onemax/Main.hs
+++ hs/onemax/Main.hs
@@ -1,4 +1,119 @@
 module Main where
 
+import System.Random
+import Data.List
+import Data.Ord
+import Control.Monad
+import Text.Printf
+
+type Individual = [Bool]
+                
+type Population = [Individual]
+
+evolve :: Int -> Population -> IO Population
+evolve 0 population = return population
+evolve generation population
+  | reachMaxFitness population = return population
+  | otherwise = do
+      printf "generation: %d highest: %d\n" generation (highestFitness population)
+      gen0 <- getStdGen
+      next <- createNextGeneration population gen0
+      -- outputPopulation next
+
+      gen1 <- getStdGen
+      let selected = select 20 (population ++ next) gen1
+      evolve (generation - 1) selected
+
+  where
+    reachMaxFitness :: Population -> Bool
+    reachMaxFitness p@(individual:_) = highestFitness p == length individual
+
+createPopulation :: Int -> Int -> StdGen -> Population
+createPopulation dim n gen = splitRecursive dim n $ take (dim * n) $ boolList gen
+  where boolList :: StdGen -> [Bool]
+        boolList = randoms
+
+        splitRecursive :: Int -> Int -> [Bool] -> [[Bool]]
+        splitRecursive dim 1 bs = [bs]
+        splitRecursive dim n bs = let (individual, bs') = splitAt dim bs
+                                  in individual:(splitRecursive dim (n - 1) bs')
+
+fitness :: Individual -> Int
+fitness = sum . map fromEnum
+
+findElite :: Population -> Individual
+findElite population = population !! maxIndex
+  where (maxIndex, _) = maximumBy (comparing snd) $ zip [0..] $ map fitness population
+
+highestFitness :: Population -> Int
+highestFitness population = fitness $ findElite population
+
+createNextGeneration :: Population -> StdGen -> IO Population
+createNextGeneration population gen = do
+  let seeds = take 20 $ randomRs (1, 100) gen
+  return $ foldl (createChild population) [] $ map mkStdGen seeds
+  where
+    createChild :: Population -> Population -> StdGen -> Population
+    createChild population acc gen = let (ratio, gen') = randomRatio gen
+                                         child = if ratio < 95 then cross population gen' else mutate population gen'
+                                     in child:acc
+    randomRatio :: StdGen -> (Int, StdGen)
+    randomRatio = randomR (1, 100)
+
+select :: Int -> Population -> StdGen -> Population
+select n population gen = map (population !!) indexes
+  where
+    fitnesses = map fitness population
+    fitnessTotal = fromIntegral $ sum fitnesses
+    thresholds = take n $ randomRs (0, fitnessTotal) gen 
+    cumlativeSum = scanl1 (+) fitnesses
+    indexes = map (\t -> fst $ head $ dropWhile (\(i, b) -> b < t) $ zip [0..] cumlativeSum) thresholds
+
+cross :: Population -> StdGen -> Individual
+cross population gen0 = child
+  where
+    (index1, gen1) = randomR (0, length population - 1) gen0
+    (index2, gen2) = randomR (0, length population - 1) gen1
+    parent1 = population !! index1
+    parent2 = population !! index2
+
+    dim = length parent1
+    (index3, gen3) = randomR (0, dim - 1) gen2
+    (index4, gen4) = randomR (0, dim - 1) gen3
+    (min, max) = if index3 < index4 then (index3, index4) else (index4, index3)
+    child = slice 0 min parent1 ++ slice min max parent2 ++ slice max dim parent1
+
+mutate :: Population -> StdGen -> Individual
+mutate population gen0 = child
+  where
+    (index1, gen1) = randomR (0, length population - 1) gen0
+    parent = population !! index1
+
+    dim = length parent
+    (index2, gen2) = randomR (0, dim - 1) gen1
+    gene = parent !! index2
+    child = slice 0 index2 parent ++ [not gene] ++ slice (index2 + 1) dim parent
+
+slice :: Int -> Int -> [a] -> [a]
+slice from to xs = take (to - from) $ drop from xs
+
+outputPopulation :: Population -> IO ()
+outputPopulation population = do
+    print "------------------------------"
+    mapM (print . map fromEnum) population
+    print "------------------------------"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    print "start"
+    gen <- getStdGen
+    finalPopulation <- evolve 200 $ createPopulation 10 40 gen
+    print $ map fitness finalPopulation
+    -- outputPopulation finalPopulation
+    print "end"
+
+
+
+
+
+
--- hs/onemax/default.nix
+++ hs/onemax/default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, random, stdenv }:
 mkDerivation {
   pname = "onemax";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ base random ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
