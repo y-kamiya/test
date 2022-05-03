37a887f1284f75e82ef81b28e8bb590143da8cc4
Yuji Kamiya <y.kamiya0@gmail.com>
2022-05-03 21:38:17 +0900

    LIS

---
+++ cpp/atcoder/dp/aoj-dpl1d/Main.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+using ll = long long;
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define REPR(i,n) for(int i=n;i>=0;i--)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define FORR(i,a,b) for(int i=(a);i>=(b);--i)
+#define ALL(x) (x).begin(),(x).end()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+#define YES(n) cout << ((n) ? "YES" : "NO"  ) << endl
+#define Yes(n) cout << ((n) ? "Yes" : "No"  ) << endl
+#define PRINT_DOUBLE(n, x) cout << std::fixed << std::setprecision(n) << x << endl;
+
+void print() { cout << endl; }
+template<typename Head, typename... Tail>
+void print(Head h, Tail... t) {
+    cout << h << " "; print(t...);
+}
+template<typename T, typename... Tail>
+void print(vector<T> vec, Tail... t) {
+    cout << "[";
+    for (const auto &e : vec) {
+        cout << e << ", ";
+    }
+    cout << "] ";
+    print(t...);
+}
+#ifdef _DEBUG
+#define DEBUG(...) print(__VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+
+
+template<typename T>
+class SegmentTree {
+    int n = 1;
+    vector<T> tree;
+
+    T empty = 0;
+    T op(T a, T b) { return max(a, b); }
+    void update_leaf(T &leaf, T val) {
+        leaf = val;
+    }
+
+public:
+    SegmentTree(int size) {
+        while (n < size) n <<= 1;
+        tree.resize(2*n-1, empty);
+    }
+
+    void update(int x, T val) {
+        x += (n-1);
+        update_leaf(tree[x], val);
+        while (x > 0) {
+            x = (x-1)/2;
+            tree[x] = op(tree[2*x+1], tree[2*x+2]);
+        }
+    }
+
+    T query(int a, int b, int k=0, int l=0, int r=-1) {
+        if (r < 0) r = n;
+        if (b <= l || r <= a) return empty;
+        if (a <= l && r <= b) return tree[k];
+
+        int mid = (l+r)/2;
+        auto vl = query(a, b, 2*k+1, l, mid);
+        auto vr = query(a, b, 2*k+2, mid, r);
+        return op(vl, vr);
+    }
+};
+
+void _main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    set<int> s;
+    REP(i, n) {
+        cin >> a[i];
+        s.insert(a[i]);
+    }
+
+    unordered_map<int, int> order;
+    vector<int> b(ALL(s));
+    REP(i, b.size()) {
+        order[b[i]] = i;
+    }
+
+    vector<int> dp(n+1, 0);
+
+    SegmentTree<int> seg(n);
+    FOR(i, 0, n) {
+        int o = order[a[i]];
+        dp[o] = seg.query(0, o) + 1;
+        seg.update(o, dp[o]);
+        DEBUG(dp);
+    }
+
+    int ans = seg.query(0, n);
+    cout << ans << endl;
+}
+
+int main() {
+    _main();
+    return 0;
+}
+
