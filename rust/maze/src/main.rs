4d754b625988ed94a4ab15b9ca3b6233d9754900
y-kamiya <y.kamiya0@gmail.com>
2025-04-01 21:37:01 +0900

    impl display

--- rust/maze/src/main.rs
+++ rust/maze/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
 struct Pos {
@@ -26,27 +27,74 @@ struct Node {
     pos: Pos,
 }
 
-
 #[derive(Debug)]
-struct Field(HashMap<Pos, Node>);
+struct Field {
+    field: HashMap<Pos, Node>,
+    start: Pos,
+    goal: Pos,
+    width: usize,
+    height: usize,
+}
 
 impl Field {
     fn new(data: &[&str]) -> Field {
         let mut map = HashMap::new();
+        let mut start: Option<Pos> = None;
+        let mut goal: Option<Pos> = None;
+
         for (y, row) in data.iter().enumerate() {
             for (x, c) in row.chars().enumerate() {
                 let node_type = match c {
                     'S' => NodeType::Start,
                     'G' => NodeType::Goal,
-                    '#' => NodeType::Wall,
                     '.' => NodeType::Road,
-                    _ => continue, //assert!(false, "invalid character of {}", c),
+                    _ => NodeType::Wall,
                 };
                 let pos = Pos::new(x, y);
+                if node_type == NodeType::Start {
+                    start = Some(pos);
+                }
+                if node_type == NodeType::Goal {
+                    goal = Some(pos);
+                }
                 map.insert(pos, Node { node_type: node_type, pos: pos });
             }
         }
-        Self(map)
+        assert!(start.is_some(), "S not found");
+        assert!(goal.is_some(), "G not found");
+
+        let h = data.len();
+        assert!(h > 1, "data must have at least 2 rows");
+        let w = data[0].len();
+        assert!(w > 1, "data must have at least 2 cols");
+
+        Self {
+            field: map,
+            start: start.unwrap(),
+            goal: goal.unwrap(),
+            width: w,
+            height: h,
+        }
+    }
+}
+
+impl fmt::Display for Field {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let pos = Pos::new(x, y);
+                let node = self.field.get(&pos).unwrap();
+                let c = match node.node_type {
+                    NodeType::Start => 'S',
+                    NodeType::Goal => 'G',
+                    NodeType::Wall => '#',
+                    NodeType::Road => '.',
+                };
+                write!(f, "{}", c)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
     }
 }
 
@@ -62,5 +110,6 @@ fn main() {
         "#######",
     ];
     let field = Field::new(&field_sample);
-    println!("{:?}", field);
+    println!("{}", field);
+
 }
